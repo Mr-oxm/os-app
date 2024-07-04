9b5b9741e3dfb3c7e5d46bdf0ebd7ea6bea56b72
Mr-oxm <88877318+Mr-oxm@users.noreply.github.com>
2024-07-04 13:15:44 +0300

    First

---
+++ .history/lib/Hooks/text_20240704125804.tsx
@@ -0,0 +1,50 @@
+// hooks/useCookie.ts
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+interface SystemSettings {
+    firstboot: boolean;
+    theme: 'light' | 'dark';
+    wallpaper: string;
+    iconsType: string;
+    topbarType: string;
+    taskbarType: string;
+    mainbodyType: string;
+    fontType: string;
+    cursorType: string;
+    bootAnimation: string;
+}
+
+const defaultSettings: SystemSettings = {
+    firstboot: true,
+    theme: 'light',
+    wallpaper: 'w_1.jpg',
+    iconsType: 'default',
+    topbarType: 'default',
+    taskbarType: 'default',
+    mainbodyType: 'default',
+    fontType: 'default',
+    cursorType: 'default',
+    bootAnimation: 'default',
+};
+
+export const useCookie = () => {
+  const [settings, setSettings] = useState<SystemSettings>(defaultSettings);
+
+  useEffect(() => {
+    const cookieData = Cookies.get('systemSettings');
+    if (cookieData) {
+      setSettings(JSON.parse(cookieData));
+    } else {
+      Cookies.set('systemSettings', JSON.stringify(defaultSettings));
+    }
+  }, []);
+
+  const updateSettings = (newSettings: Partial<SystemSettings>) => {
+    const updatedSettings = { ...settings, ...newSettings };
+    setSettings(updatedSettings);
+    Cookies.set('systemSettings', JSON.stringify(updatedSettings));
+  };
+
+  return { settings, updateSettings };
+};
---
+++ .history/lib/Store/useWallpaperStore_20240704130540.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { useSettings } from '@/hooks/useSettings';
+
+type WallpaperState = {
+  wallpaper: string;
+  setWallpaper: (wallpaper: string) => void;
+};
+
+export const useWallpaperStore = create<WallpaperState>()(
+  persist(
+    (set) => {
+      const { settings, updateSettings } = useSettings();
+      return {
+        wallpaper: settings?.wallpaper || 'w_1.jpg',
+        setWallpaper: (newWallpaper) => {
+          set({ wallpaper: newWallpaper });
+          updateSettings({ wallpaper: newWallpaper });
+        },
+      };
+    },
+    {
+      name: 'wallpaper-storage',
+    }
+  )
+);
