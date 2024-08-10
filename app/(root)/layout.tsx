2b7e30a0d99bdf1d801a79a5cab2561181f011c6
Mr-oxm <88877318+Mr-oxm@users.noreply.github.com>
2024-08-10 15:43:24 +0300

    favicon

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <head>
-        <link rel="icon" href="/icons/os_icon2.svg" sizes="any" />
+        <link rel="icon" href="./icons/os_icon2.svg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
